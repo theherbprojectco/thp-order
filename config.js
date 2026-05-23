5b2c4548575ef075a2ef3ccbf68626448a321c25
theherbprojectco <theherbprojectco@gmail.com>
2026-05-23 17:03:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ORDERFLOW_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxm-3m3epn3CcGb9gDo5SmVSCAuuE24OtIpiolB2oNoUzYBxV_YyhMZSf1mUoAben6mTw/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyeo_ZX9anlyHCD4DEiRY8WQld-yyqtNBdBc1r7gfZScNdRqxpukJdB645slEvAgRD9gQ/exec",
   adminPin: "9836",
   teamPin: "8296",
 };
